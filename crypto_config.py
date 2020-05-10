3e72568a23f0b4f534c5422f6d4851785daa530a
Michael Stenger <michael.stenger@outlook.de>
2020-05-10 21:54:56 +0200

    cleaning up

--- crypto_config.py
+++ crypto_config.py
@@ -5,6 +5,6 @@ Created on Mon Nov 11 18:28:39 2019
 @author: Michi
 """
 
-path_to_input_file  = "/home/michi/hochzeit/development-homepage/index_dev.html"
-path_to_output_file = "/home/michi/hochzeit/sabine-und-michael.github.io/website_2.js"
-password            = b"lebenslang"
+path_to_input_file  = ""
+path_to_output_file = ""
+password            = b""
